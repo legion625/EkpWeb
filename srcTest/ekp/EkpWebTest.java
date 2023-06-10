6e2ce5495adce14bfc862db5ed96ccb09a4b38dc
Min-Hua Chao <s106034803@m106.nthu.edu.tw>
2023-06-10 21:53:26 +0800

    [lib]
    1. legionmodule: 0.12.3 -> 0.13.0
      -1. httpcomponents-client-4.5.6
      -2. json
      -3. gson_2.8.5

--- srcTest/ekp/EkpWebTest.java
+++ srcTest/ekp/EkpWebTest.java
@@ -5,12 +5,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import legion.SystemInfoDefault;
+import legion.openai.ChatBot;
 
 public class EkpWebTest extends AbstractEkpInitTest{
-	private static Logger log = LoggerFactory.getLogger(EkpWebTest.class);
+	private static Logger log = LoggerFactory.getLogger(TestLogMark.class);
 	@Test
 	public void test0() {
 		log.debug("test0");
 		log.debug("SystemInfoDefault.getInstance().getVersion(): {}", SystemInfoDefault.getInstance().getVersion());
+	}
+
+	@Test
+	public void testOpenaiChatBot() {
+//		String apiKey = "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";
+		String apiKey = "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";
+		
+		String input = "請幫我解析這段話的intent和entity:”55A3-Z0多少錢？”";
+		log.debug("input: {}", input);
+		String output = ChatBot.sendQuery(input, apiKey);
+		log.debug("output: {}", output);
+
 	}
 }
